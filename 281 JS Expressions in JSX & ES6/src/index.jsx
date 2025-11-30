8c99e5028598b67ecf18a2fe5da010b646459d7f
Ryan Reed <ryno5454@gmail.com>
2025-11-29 18:46:11 -0600

    Refactor name variables and update JSX rendering

--- 281 JS Expressions in JSX & ES6/src/index.jsx
+++ 281 JS Expressions in JSX & ES6/src/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const name = "Ryan";
+const fName = "Ryan";
+const lName ="Reed";
 const num = "67";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <h1>Hello {name}!</h1>
+<div>
+    <h1>Hello {fName + " " + lName}!</h1>
     <p>My lucky number is {num}</p>
-  </div>
+</div>
 );
