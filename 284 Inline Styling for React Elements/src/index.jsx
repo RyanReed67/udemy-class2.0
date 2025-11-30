636104230348708e0dc84a3e4fc618195be5f3a1
Ryan Reed <ryno5454@gmail.com>
2025-11-30 10:59:12 -0600

    Add files via upload

---
+++ 284 Inline Styling for React Elements/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<h1 style={customStyle}>Hello World!</h1>);
